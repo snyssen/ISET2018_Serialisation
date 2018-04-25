ecc5ffcef25e8cc6be3b289721caf4ac9da25839
snyssen <36501744+snyssen@users.noreply.github.com>
2018-04-25 13:29:10 +0200

    Add project files.

---
+++ ISET2018_Serialisation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISET2018_Serialisation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
