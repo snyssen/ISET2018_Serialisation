f7d57dfeae68804ee6c0efb5af8c7870cbcee9ac
Simon NYSSEN <Simon.NYSSEN@outlook.be>
2018-06-11 18:02:08 +0200

    + Commentée

--- ISET2018_Serialisation/Program.cs
+++ ISET2018_Serialisation/Program.cs
@@ -51,20 +51,36 @@ namespace ISET2018_Serialisation
             Console.ReadLine();
         }
     }
-    [Serializable]
-    [XmlRoot()]
+
+    /// <summary>
+    /// Ressort un xml de type :
+    /// 
+    ///     <?xml version="1.0" encoding="utf-8"?>
+    ///     <PersonneSerialisee xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" Identifiant="1">
+    ///       <Nom>Winch</Nom>
+    ///       <Prénom>Largo</Prénom>
+    ///       <Liste>
+    ///         <Conquête>Danitza</Conquête>
+    ///         <Conquête>Charity</Conquête>
+    ///         <Conquête>Marilyn</Conquête>
+    ///       </Liste>
+    ///     </PersonneSerialisee>
+    ///
+    /// </summary>
+    [Serializable] // On indique la classe comme sérialisable
+    [XmlRoot()] // Indique "PersonneSerialisee" comme balise racine (=> celle qui encadre toute la classe) 
     public class PersonneSerialisee
     {
-        [XmlAttribute("Identifiant")]
+        [XmlAttribute("Identifiant")] // ajoute l'identifiant DANS la balise précédente (donc ici root => PersonneSerialisee)
         public int ID { get; set; }
-        [XmlElement("Nom")]
+        [XmlElement("Nom")] // Ajoute une balise "Nom" pour encadrer la variable Nom
         public string Nom { get; set; }
-        [XmlElement("Prénom")]
+        [XmlElement("Prénom")] // idem
         public string Prenom { get; set; }
-        [XmlIgnore]
+        [XmlIgnore] // Indique que la sérialisation doit ignorer cette variable => Elle ne sera pas sauvée dans le xml
         public DateTime Naissance { get; set; }
-        [XmlArray("Liste")]
-        [XmlArrayItem("Conquête")]
+        [XmlArray("Liste")] // Indique une balise qui encadre un "bloc", ici notre liste de conquête
+        [XmlArrayItem("Conquête")] // Indique que notre liste doit être sérialisée item par item dans le bloc "Liste", avec pour chaque item la balise "Conquête"
         public List<string> Lst { get; set; }
         public PersonneSerialisee()
         {
@@ -98,6 +114,38 @@ namespace ISET2018_Serialisation
             }
         }
     }
+
+    /// <summary>
+    /// Ressort un xml de type :
+    /// SerialiserFichier (En une seule ligne dans le vrai fichier) ->
+    /// 
+    ///      <?xml version="1.0" encoding="utf-8"?>
+    ///      <Personne Identifiant="1">
+    ///          <Prénom>Largo</Prénom>
+    ///          <Nom>Winch</Nom>
+    ///          <Liste>
+    ///              <Conquête>Danitza</Conquête>
+    ///              <Conquête>Charity</Conquête>
+    ///              <Conquête>Marilyn</Conquête>
+    ///          </Liste>
+    ///      </Personne>
+    /// 
+    /// SerialiserToutFichier ->
+    /// 
+    ///     <?xml version="1.0" encoding="utf-8"?>
+    ///     <Personne xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    ///       <ID>1</ID>
+    ///       <Nom>Winch</Nom>
+    ///       <Prenom>Largo</Prenom>
+    ///       <Naissance>1975-07-15T00:00:00</Naissance>
+    ///       <Lst>
+    ///         <string>Danitza</string>
+    ///         <string>Charity</string>
+    ///         <string>Marilyn</string>
+    ///       </Lst>
+    ///     </Personne>
+    /// 
+    /// </summary>
     public class Personne
     {
         public int ID { get; set; }
@@ -118,22 +166,22 @@ namespace ISET2018_Serialisation
         }
         public void SerialiserFichier(string nf)
         {
-            using (XmlTextWriter xw = new XmlTextWriter(nf, Encoding.UTF8))
+            using (XmlTextWriter xw = new XmlTextWriter(nf, Encoding.UTF8)) // Utilisation d'un writer en xml
             {
-                xw.WriteStartDocument();
-                xw.WriteStartElement("Personne");
-                xw.WriteAttributeString("Identifiant", ID.ToString());
-                xw.WriteElementString("Prénom", Prenom);
-                xw.WriteElementString("Nom", Nom);
-                xw.WriteStartElement("Liste");
-                foreach(string l in Lst)
+                xw.WriteStartDocument(); // Démarre le doc -> Ecrit la version xml et son encodage (utf8)
+                xw.WriteStartElement("Personne"); // Commence par la balise "Personne"... => StartElement écrit une balise qui va encadrer un bloc
+                xw.WriteAttributeString("Identifiant", ID.ToString()); // ...A laquelle on attache l'ID
+                xw.WriteElementString("Prénom", Prenom); // Balise "Prénom"
+                xw.WriteElementString("Nom", Nom); // Balise "Nom"
+                xw.WriteStartElement("Liste"); // A nouveau une balise 'bloc' => Encadre la liste
+                foreach (string l in Lst)
                 {
-                    xw.WriteElementString("Conquête", l);
+                    xw.WriteElementString("Conquête", l); // Eléments de la liste
                 }
-                xw.WriteEndElement();
-                xw.WriteEndElement();
-                xw.WriteEndDocument();
-                xw.Close();
+                xw.WriteEndElement(); // Ferme le bloc liste
+                xw.WriteEndElement(); // Ferme le bloc "Personne"
+                xw.WriteEndDocument(); // Ferme le document
+                xw.Close(); // Ferme la "connexion" au document
             }
         }
         public static Personne DeSerialiserFichier(string nf)
@@ -165,7 +213,7 @@ namespace ISET2018_Serialisation
         {
             XmlSerializer xs = new XmlSerializer(this.GetType());
             StreamWriter sw = new StreamWriter(nf);
-            xs.Serialize(sw, this);
+            xs.Serialize(sw, this); // Sérialise toutes les données de la classe sans distinction, avec les noms des variables comme noms de balises
             sw.Close();
         }
         public static Personne DeSerialiserToutFichier(string nf)
@@ -177,6 +225,10 @@ namespace ISET2018_Serialisation
             return rep;
         }
     }
+
+    /// <summary>
+    /// Identique aux fonctions utilisées dans "Personne" mais générique pour être utilisable par n'importe quelle classe
+    /// </summary>
     public class UtilitairesSerialisation
     {
         public static void SerialiserFichier<T>(string nf, T obj)
@@ -195,4 +247,15 @@ namespace ISET2018_Serialisation
             return rep;
         }
     }
+
+    /*
+     *  POUR RESUMER :
+     *  
+     *  - PersonneSerialise utilise des instructions [] pour indiquer les balises xml des variables DANS LA CLASSE
+     *  - Personne utilise :
+     *                         1) une sérialisation "manuelle", élément par élément en indiquant nous même les balises et ce qu'il faut mettre entre elles
+     *                         2) une sérialisation "automatique", qui prend toutes les variables de la classe et les insère dans le xml avec leurs noms comme balises
+     *  - UtilitairesSerialisation permet d'utiliser la méthode automatique de Personne, mais de manière générique => Fonctionne pour n'importe quelle classe
+     *
+    */
 }
